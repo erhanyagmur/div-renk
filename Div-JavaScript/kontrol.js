474c9b4e966b5031e7296c19bebcbce852543f1b
erhanyagmur <119954665+erhanyagmur@users.noreply.github.com>
2023-03-08 19:53:26 +0300

    Add files via upload

---
+++ Div-JavaScript/kontrol.js
@@ -0,0 +1,34 @@
+function ders1a()
+{
+	document.getElementById("ders1").style.backgroundColor="blue";
+	document.getElementById("ders1").style.color="white";
+}
+function ders1b()
+{
+	document.getElementById("ders1").style.backgroundColor="brown";
+	document.getElementById("ders1").style.color="white";
+}
+function ders2a()
+{
+	document.getElementById("ders2").style.backgroundColor="green";
+	document.getElementById("ders2").style.color="white";
+}
+function ders2b()
+{
+	document.getElementById("ders2").style.backgroundColor="blue";
+	document.getElementById("ders2").style.color="white";
+}
+function ders3a()
+{
+	document.getElementById("ders3").style.backgroundColor="black";
+	document.getElementById("ders3").style.color="white";
+}
+function ders3b()
+{
+	document.getElementById("ders3").style.backgroundColor="aqua";
+	document.getElementById("ders3").style.color="white";
+}
+function yukle()
+{
+	document.getElementsByTagName("body")[0].style.backgroundColor="black";
+}
